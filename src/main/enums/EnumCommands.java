548106ad9f953c13e53672d2735b06cb0cb1d215
Sébastien.Degand <sebastien.degand@etu.unice.fr>
2017-05-02 13:58:39 +0100

    seb: packages

---
+++ src/main/enums/EnumCommands.java
@@ -0,0 +1,23 @@
+package main.enums;
+
+/**
+ * Created by Quentin on 01/05/2017.
+ */
+public enum EnumCommands {
+
+    Close("CLOSE"),
+    PostIdea("POSTIDEA"),
+    ListIdea("LISTIDEA"),
+    PostIdeaResponse("POSTIDEARESPONSE"),
+    ListIdeaResponse("LISTIDEARESPONSE");
+
+    private String command;
+
+    EnumCommands(String command){
+        this.command = command;
+    }
+
+    public String toString(){
+        return command;
+    }
+}
